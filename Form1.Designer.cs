ddf7fd4dbf2f48777527a8204f6cf3d14b8263b4
elchinbaba <babalielchin@gmail.com>
2022-12-11 20:32:13 +0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+namespace CourseFinder
+{
+    partial class CourseFinder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cBoxCategory = new System.Windows.Forms.ComboBox();
+            this.cBoxBudget = new System.Windows.Forms.ComboBox();
+            this.cBoxRegime = new System.Windows.Forms.ComboBox();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.pBoxInstitution = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxInstitution)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cBoxCategory
+            // 
+            this.cBoxCategory.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cBoxCategory.FormattingEnabled = true;
+            this.cBoxCategory.Items.AddRange(new object[] {
+            "programming",
+            "business",
+            "language"});
+            this.cBoxCategory.Location = new System.Drawing.Point(75, 92);
+            this.cBoxCategory.Name = "cBoxCategory";
+            this.cBoxCategory.Size = new System.Drawing.Size(330, 33);
+            this.cBoxCategory.TabIndex = 0;
+            // 
+            // cBoxBudget
+            // 
+            this.cBoxBudget.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cBoxBudget.FormattingEnabled = true;
+            this.cBoxBudget.Items.AddRange(new object[] {
+            "low",
+            "mid",
+            "high"});
+            this.cBoxBudget.Location = new System.Drawing.Point(75, 169);
+            this.cBoxBudget.Name = "cBoxBudget";
+            this.cBoxBudget.Size = new System.Drawing.Size(330, 33);
+            this.cBoxBudget.TabIndex = 1;
+            // 
+            // cBoxRegime
+            // 
+            this.cBoxRegime.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cBoxRegime.FormattingEnabled = true;
+            this.cBoxRegime.Items.AddRange(new object[] {
+            "online",
+            "offline",
+            "hybrid"});
+            this.cBoxRegime.Location = new System.Drawing.Point(75, 253);
+            this.cBoxRegime.Name = "cBoxRegime";
+            this.cBoxRegime.Size = new System.Drawing.Size(330, 33);
+            this.cBoxRegime.TabIndex = 2;
+            // 
+            // btnFind
+            // 
+            this.btnFind.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFind.Location = new System.Drawing.Point(172, 334);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(138, 58);
+            this.btnFind.TabIndex = 3;
+            this.btnFind.Text = "Find";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(557, 136);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(93, 25);
+            this.lblResult.TabIndex = 4;
+            this.lblResult.Text = "institution";
+            this.lblResult.Visible = false;
+            // 
+            // pBoxInstitution
+            // 
+            this.pBoxInstitution.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pBoxInstitution.Location = new System.Drawing.Point(482, 199);
+            this.pBoxInstitution.Name = "pBoxInstitution";
+            this.pBoxInstitution.Size = new System.Drawing.Size(244, 146);
+            this.pBoxInstitution.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBoxInstitution.TabIndex = 5;
+            this.pBoxInstitution.TabStop = false;
+            // 
+            // CourseFinder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pBoxInstitution);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.cBoxRegime);
+            this.Controls.Add(this.cBoxBudget);
+            this.Controls.Add(this.cBoxCategory);
+            this.Name = "CourseFinder";
+            this.Text = "Find Course";
+            this.Load += new System.EventHandler(this.CourseFinder_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxInstitution)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cBoxCategory;
+        private System.Windows.Forms.ComboBox cBoxBudget;
+        private System.Windows.Forms.ComboBox cBoxRegime;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.PictureBox pBoxInstitution;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+
+using CLIPSNET;
+
+namespace CourseFinder
+{
+    public partial class CourseFinder : Form
+    {
+        private CLIPSNET.Environment clips = new CLIPSNET.Environment();
+        private Dictionary<string, string> courseImages = new Dictionary<string, string>();
+
+        public CourseFinder()
+        {
+            InitializeComponent();
+
+            courseImages.Add("STEP-IT", "\\images\\" + "STEP-IT.png");
+            courseImages.Add("IT-ACADEMY", "\\images\\" + "IT-ACADEMY.png");
+            courseImages.Add("SAT-ACADEMY", "\\images\\" + "SAT-ACADEMY.png");
+            courseImages.Add("THINK-WISE", "\\images\\" + "THINK-WISE.png");
+            courseImages.Add("LLBS", "\\images\\" + "LLBS.jpg");
+            courseImages.Add("CELT-COLLEGES", "\\images\\" + "CELT-COLLEGES.png");
+        }
+
+
+        private void CourseFinder_Load(object sender, EventArgs e)
+        {
+            cBoxCategory.SelectedIndex = 0;
+            cBoxBudget.SelectedIndex = 0;
+            cBoxRegime.SelectedIndex = 0;
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            pBoxInstitution.Image = null;
+            clips.Clear();
+
+            clips.Load("courses.clp");
+            clips.Load("program.clp");
+
+            clips.Reset();
+
+            String criteriaCategory = cBoxCategory.Text;
+            String criteriaBudget = cBoxBudget.Text;
+            String criteriaRegime = cBoxRegime.Text;
+
+            clips.AssertString($"(category {criteriaCategory})");
+            clips.AssertString($"(budget {criteriaBudget})");
+            clips.AssertString($"(regime {criteriaRegime})");
+
+            clips.Run();
+
+            try
+            {
+                string institution = clips.FindFact("result")["institution"].ToString();
+                lblResult.Text = institution;
+                pBoxInstitution.Image = Image.FromFile(Directory.GetCurrentDirectory() + courseImages[institution]);
+            }
+            catch
+            {
+                lblResult.Text = "sorry, no result";
+            }
+            lblResult.Visible = true;
+        }
+    }
+}
